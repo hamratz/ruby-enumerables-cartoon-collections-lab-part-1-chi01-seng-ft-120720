3ecbfd91275ad105014415358f4918483408dc0c
Marty Warfel <marthawarfel@gmail.com>
2020-11-29 04:44:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,12 +3,10 @@ def greet_characters(array)
   array.each do |character|
    puts "Hello #{character.add}!"
  end
- character += 1 
+
 end
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
-end
+ 
+
 
 def 
     
