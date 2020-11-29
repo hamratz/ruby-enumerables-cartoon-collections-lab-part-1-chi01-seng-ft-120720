941f5d7a2888c0f944afbc717acea9954564b250
Marty Warfel <marthawarfel@gmail.com>
2020-11-29 04:43:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   array.each do |character|
    puts "Hello #{character.add}!"
  end
- array += 1 
+ character += 1 
 end
   # Use `each` to enumerate over the provided array
   #
